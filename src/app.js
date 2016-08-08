dce19fa641d5e842cdf985ac4649f8e26cfc66e8
Ben Clinkinbeard <ben.clinkinbeard@gmail.com>
2016-08-08 15:06:53 -0400

    Time scales

--- src/app.js
+++ src/app.js
@@ -1,6 +1,9 @@
-var linearScale = d3.scaleLinear()
-  .domain([0, 100])
-  .range([0, 600])
-  .clamp(true);
+var timeScale = d3.scaleTime()
+  .domain([new Date(2016, 0, 1), new Date()])
+  .range([0, 100]);
 
-console.log(linearScale(200));
+console.log(timeScale(new Date(2016, 0, 15)));
+console.log(timeScale(new Date(2016, 3, 15)));
+console.log(timeScale(new Date()));
+
+console.log(timeScale.invert(50));
