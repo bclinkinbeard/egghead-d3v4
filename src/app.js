e7b3c75bc6bf7652388df0ac02728c2084fcb51b
Ben Clinkinbeard <ben.clinkinbeard@gmail.com>
2016-08-09 10:45:16 -0400

    05-d3-scale-quantize

--- src/app.js
+++ src/app.js
@@ -1,6 +1,9 @@
-var linearScale = d3.scaleLinear()
+var quantizeScale = d3.scaleQuantize()
   .domain([0, 100])
-  .range([0, 600])
-  .clamp(true);
+  .range(["red", "white", "green"]);
 
-console.log(linearScale(200));
+console.log(quantizeScale(22));
+console.log(quantizeScale(50));
+console.log(quantizeScale(88));
+
+console.log(quantizeScale.invertExtent('white'));
