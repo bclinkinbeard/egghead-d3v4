8f46a48d36f17738792eb2a374c64c6b083d1b3e
Ben Clinkinbeard <ben.clinkinbeard@gmail.com>
2016-08-10 13:01:33 -0400

    09-d3-selection-modifying-elements

--- src/app.js
+++ src/app.js
@@ -1,11 +1,4 @@
-var div = d3.select('div');
-console.log(div.nodes());
-
-var divLinks = div.selectAll('a');
-console.log(divLinks.nodes());
-
-var secondLink = d3.selectAll('a:nth-child(2)');
-console.log(secondLink.nodes());
-
-var allLinks = d3.selectAll(document.links);
-console.log(allLinks.size());
+d3.selectAll('a:nth-child(2)')
+  .attr('href', 'http://google.com')
+  .classed('red', true)
+  .html('Inventory <b>SALE</b>');
