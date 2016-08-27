f85c17ff3cdba197a348b3e1aeb078b68a934043
Ben Clinkinbeard <ben.clinkinbeard@gmail.com>
2016-08-26 21:40:49 -0400

    12-output-types

--- src/app.js
+++ src/app.js
@@ -6,24 +6,17 @@ var scores = [
   { name: 'Emily', score: 88 }
 ];
 
-var update = d3.select('.chart')
-  .selectAll('div')
-  .data(scores, function (d) {
-    return d ? d.name : this.innerText;
-  })
-  .style('color', 'blue');
-
-var enter = update.enter()
-  .append('div')
-  .text(function (d) {
-    return d.name;
-  })
-  .style('color', 'green');
-
-update.exit().remove();
-
-update.merge(enter)
-  .style('width', d => d.score + 'px')
-  .style('height', '50px')
-  .style('background', 'lightgreen')
-  .style('border', '1px solid black')
+d3.select('.chart')
+  .append('svg')
+    .attr('width', 225)
+    .attr('height', 300)
+  .selectAll('rect')
+  .data(scores)
+  .enter()
+    .append('rect')
+    .attr('y', (d, i) => i * 33)
+    .style('width', d => d.score)
+    .text(function (d) {
+      return d.name;
+    })
+    .attr('class', 'bar');
