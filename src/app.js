bfcbecd713219394afe6283b0a903c06361945dc
Ben Clinkinbeard <ben.clinkinbeard@gmail.com>
2016-08-30 13:55:04 -0400

    15-selection-call

--- src/app.js
+++ src/app.js
@@ -16,19 +16,37 @@ var bar = d3.select('.chart')
     .append('g')
     .attr('transform', (d, i) => 'translate(0, ' + i * 33 + ')');
 
+function scaleBar (selection, scale) {
+  selection.style('transform', 'scaleX(' + scale + ')');
+}
+
+function setFill (selection, color) {
+  selection.style('fill', color);
+}
+
+function fade (selection, opacity) {
+  selection.style('fill-opacity', opacity);
+}
+
 bar.append('rect')
     .style('width', d => d.score)
     .attr('class', 'bar')
     .on('mouseover', function (d, i, elements) {
-      d3.select(this).style('transform', 'scaleX(2)');
+      d3.select(this)
+        .call(scaleBar, 2)
+        .call(setFill, 'orange');
+
       d3.selectAll(elements)
         .filter(':not(:hover)')
-        .style('fill-opacity', 0.5);
+        .call(fade, 0.5);
     })
     .on('mouseout', function (d, i, elements) {
-      d3.select(this).style('transform', 'scaleX(1)');
+      d3.select(this)
+        .call(scaleBar, 1)
+        .call(setFill, 'lightgreen');
+
       d3.selectAll(elements)
-        .style('fill-opacity', 1);
+        .call(fade, 1);
     });
 
 bar.append('text')
