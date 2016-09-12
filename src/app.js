4b005a3777193e2f6c30102a316e8231c854c950
Ben Clinkinbeard <ben.clinkinbeard@gmail.com>
2016-09-12 13:07:56 -0400

    Code note and README update

--- src/app.js
+++ src/app.js
@@ -1,3 +1,8 @@
+// you will see two red links when viewing this on Plunker
+// we did not see this behavior in the lesson video
+// because our BrowserSync dev server was inserting a script
+// making the last link NOT a second child
+
 d3.selectAll('a:nth-child(2)')
   .attr('href', 'http://google.com')
   .classed('red', true)
