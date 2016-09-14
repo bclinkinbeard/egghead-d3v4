38e6f81df35bada159d66a7ed1abd829865ecfd6
Ben Clinkinbeard <ben.clinkinbeard@gmail.com>
2016-09-14 13:52:47 -0400

    Update

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
-var margin = { top: 0, right: 0, bottom: 30, left: 20 };
+var margin = { top: 10, right: 20, bottom: 25, left: 25 };
 var width = 425 - margin.left - margin.right;
 var height = 625 - margin.top - margin.bottom;
 
@@ -7,10 +7,17 @@ var svg = d3.select('.chart')
     .attr('width', width + margin.left + margin.right)
     .attr('height', height + margin.top + margin.bottom)
   .append('g')
-    .attr('transform', 'translate(' + margin.left + ', ' + margin.top + ')');
+    .attr('transform', `translate(${margin.left}, ${margin.top})`);
 
 svg.append('rect')
-  .attr('width', width)
+  .attr('width', width / 2)
+  .attr('height', height)
+  .style('fill', 'lightblue')
+  .style('stroke', 'green');
+
+svg.append('rect')
+  .attr('x', width / 2)
+  .attr('width', width / 2)
   .attr('height', height)
   .style('fill', 'lightblue')
   .style('stroke', 'green');
