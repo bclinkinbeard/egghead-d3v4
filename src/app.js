d7b9188f16c9e891b77c5a82dced71403206f620
Ben Clinkinbeard <ben.clinkinbeard@gmail.com>
2016-09-30 15:16:36 -0400

    19-scatter-plot

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
-var margin = { top: 10, right: 20, bottom: 60, left: 30 };
+var margin = { top: 10, right: 20, bottom: 30, left: 30 };
 var width = 400 - margin.left - margin.right;
 var height = 565 - margin.top - margin.bottom;
 
@@ -10,48 +10,59 @@ var svg = d3.select('.chart')
   .append('g')
     .attr('transform', 'translate(' + margin.left + ', ' + margin.top + ')');
 
-var data = [
-  {score: 63, subject: 'Mathematics'},
-  {score: 82, subject: 'Geography'},
-  {score: 74, subject: 'Spelling'},
-  {score: 97, subject: 'Reading'},
-  {score: 52, subject: 'Science'},
-  {score: 74, subject: 'Chemistry'},
-  {score: 97, subject: 'Physics'},
-  {score: 52, subject: 'ASL'}
-];
+d3.json('./data.json', function (err, data) {
+  var yScale = d3.scaleLinear()
+    .domain(d3.extent(data, d => d.expectancy))
+    .range([height, 0])
+    .nice();
+  var yAxis = d3.axisLeft(yScale);
+  svg.call(yAxis);
 
-var yScale = d3.scaleLinear()
-  .domain([0, 100])
-  .range([height, 0]);
-var yAxis = d3.axisLeft(yScale);
-svg.call(yAxis);
+  var xScale = d3.scaleLinear()
+    .domain(d3.extent(data, d => d.cost))
+    .range([0, width])
+    .nice();
 
-var xScale = d3.scaleBand()
-  .padding(0.2)
-  .domain(data.map(d => d.subject))
-  .range([0, width]);
+  var xAxis = d3.axisBottom(xScale)
+    .ticks(5);
+  svg
+    .append('g')
+      .attr('transform', `translate(0, ${height})`)
+    .call(xAxis);
 
-var xAxis = d3.axisBottom(xScale)
-  .ticks(5)
-  .tickSize(10)
-  .tickPadding(5);
-svg
-  .append('g')
-    .attr('transform', `translate(0, ${height})`)
-  .call(xAxis)
-  .selectAll('text')
-  .style('text-anchor', 'end')
-  .attr('transform', 'rotate(-45)');
+  var rScale = d3.scaleSqrt()
+    .domain([0, d3.max(data, d => d.population)])
+    .range([0, 40]);
 
-svg.selectAll('rect')
-  .data(data)
-  .enter()
-  .append('rect')
-  .attr('x', d => xScale(d.subject))
-  .attr('y', d => yScale(d.score))
-  .attr('width', d => xScale.bandwidth())
-  .attr('height', d => height - yScale(d.score));
+  var circles = svg
+    .selectAll('.ball')
+    .data(data)
+    .enter()
+    .append('g')
+    .attr('class', 'ball')
+    .attr('transform', d => {
+      return `translate(${xScale(d.cost)}, ${yScale(d.expectancy)})`;
+    });
+
+  circles
+    .append('circle')
+    .attr('cx', 0)
+    .attr('cy', 0)
+    .attr('r', d => rScale(d.population))
+    .style('fill-opacity', 0.5)
+    .style('fill', 'steelblue');
+
+  circles
+    .append('text')
+    .style('text-anchor', 'middle')
+    .style('fill', 'black')
+    .attr('y', 4)
+    .text(d => d.code);
+
+});
+
+
+
 
 function responsivefy(svg) {
   // get container + svg aspect ratio
