fa8ccf858ce32514545a26813f2b2f304d034f3e
Ben Clinkinbeard <ben.clinkinbeard@gmail.com>
2016-10-11 15:10:50 -0400

    24-reusable-transitions

--- src/app.js
+++ src/app.js
@@ -1,15 +1,28 @@
-d3.select('#block')
-  .transition('dimensions')
-    .duration(600)
-    .ease(d3.easeBounceOut)
-    .style('width', '400px')
-  .transition()
-    .duration(600)
-    .ease(d3.easeBounceOut)
-    .style('height', '600px')
+function go () {
+  var t = d3.transition()
+    .delay(1000)
+    .duration(1000);
 
-d3.select('#block')
-  .transition('color')
-    .duration(2000)
-    .ease(d3.easeQuadOut)
-    .style('background-color', 'purple')
+  d3.selectAll('.block')
+    .transition(t)
+    .style('width', '400px');
+
+  d3.select('.a')
+    .transition(t)
+    .style('background-color', 'orange');
+
+  d3.select('.b')
+    .transition(t)
+    .style('background-color', 'blue');
+}
+
+function configure (t, delay, duration) {
+  return t.delay(delay).duration(duration);
+}
+
+function goNow () {
+  d3.selectAll('.block')
+    .transition()
+    .call(configure, 1000, 1000)
+    .style('height', '200px');
+}
