abcfe19ef4a647fe51fcb3d8ec0ffb93671fe282
Ben Clinkinbeard <ben.clinkinbeard@gmail.com>
2016-10-14 22:18:34 -0400

    Simpler demo

--- src/app.js
+++ src/app.js
@@ -1,9 +1,14 @@
-var margin = { top: 10, right: 20, bottom: 30, left: 30 };
+var data = [
+  {name: 'Alice', math: 37,   science: 62,   language: 54},
+  {name: 'Billy', math: null, science: 34,   language: 85},
+  {name: 'Cindy', math: 86,   science: 48,   language: null},
+  {name: 'David', math: 44,   science: null, language: 65},
+  {name: 'Emily', math: 59,   science: 73,   language: 29}
+];
+
+var margin = { top: 10, right: 10, bottom: 30, left: 30 };
 var width = 400 - margin.left - margin.right;
 var height = 565 - margin.top - margin.bottom;
-
-var data;
-var xScale;
 
 var svg = d3.select('.chart')
   .append('svg')
@@ -11,7 +16,16 @@ var svg = d3.select('.chart')
     .attr('height', height + margin.top + margin.bottom)
     .call(responsivefy)
   .append('g')
-    .attr('transform', 'translate(' + margin.left + ', ' + margin.top + ')');
+    .attr('transform', `translate(${margin.left}, ${margin.top})`);
+
+var xScale = d3.scaleBand()
+  .domain(data.map(d => d.name))
+  .range([0, width])
+  .padding(0.2);
+svg
+  .append('g')
+    .attr('transform', `translate(0, ${height})`)
+  .call(d3.axisBottom(xScale));
 
 var yScale = d3.scaleLinear()
   .domain([0, 100])
@@ -20,55 +34,14 @@ svg
   .append('g')
   .call(d3.axisLeft(yScale));
 
-d3.json('./data.json', function (err, result) {
-  data = result;
-
-  xScale = d3.scaleBand()
-    .domain(data.map(d => d.name).sort())
-    .range([0, width])
-    .padding(0.2);
-  svg
-    .append('g')
-    .attr('transform', `translate(0, ${height})`)
-    .call(d3.axisBottom(xScale));
-
-  render();
-});
-
-function render (subject = 'math') {
-  var t = d3.transition().duration(1000);
-
-  var update = svg
-    .selectAll('rect')
-    .data(data.filter(d => d[subject]), function (d) {
-      return d.name;
-    });
-
-  update
-    .exit()
-    .transition(t)
-    .attr('y', height)
-    .attr('height', '0')
-    .remove();
-
-  update
-    .transition(t)
-    .delay(750)
-    .attr('y', d => yScale(d[subject]))
-    .attr('height', d => height - yScale(d[subject]));
-
-  var enter = update.enter()
-    .append('rect')
-    .attr('x', d => xScale(d.name))
-    .attr('y', height)
-    .attr('width', xScale.bandwidth())
-    .attr('height', 0)
-    .style('fill', 'steelblue')
-    .transition(t)
-    .delay(update.exit().size() ? 1500 : 0)
-    .attr('y', d => yScale(d[subject]))
-    .attr('height', d => height - yScale(d[subject]));
-}
+svg.selectAll('rect')
+  .data(data)
+  .enter()
+  .append('rect')
+  .attr('x', d => xScale(d.name))
+  .attr('y', d => yScale(d.math))
+  .attr('width', d => xScale.bandwidth())
+  .attr('height', d => height - yScale(d.math));
 
 function responsivefy(svg) {
   // get container + svg aspect ratio
